c101480ab9f4ed48dfeaf7133bfe267d5c191520
maryam <marychch19@gmail.com>
2024-09-13 13:31:58 +0330

    calakeArea

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function celsiusToFahrenheit(celsius) {
-  const fahrenheit = celsius * 1.8 + 32;
-  return fahrenheit;
+// function celsiusToFahrenheit(celsius) {
+//   const fahrenheit = celsius * 1.8 + 32;
+//   return fahrenheit;
+// }
+// const celsius1 = celsiusToFahrenheit(5);
+// console.log(celsius1);
+//
+/*
+ *
+ *
+ *
+ *
+ */
+function calakeArea(length, width) {
+  const area = length * width;
+  return area;
 }
-const celsius1 = celsiusToFahrenheit(2);
-console.log(celsius1);
+const area1 = calakeArea(5, 10);
+console.log(`calakeArea = `, area1);
