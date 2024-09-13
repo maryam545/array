37a7b629bcb6f12a943c89fd9cd494a1e74981dc
maryam <marychch19@gmail.com>
2024-09-13 14:10:20 +0330

    function

--- script.js
+++ script.js
@@ -11,9 +11,30 @@
  *
  *
  */
-function calakeArea(length, width) {
-  const area = length * width;
-  return area;
+// function calakeArea(length, width) {
+//   const area = length * width;
+//   return area;
+// }
+// const area1 = calakeArea(5, 10);
+// console.log(`calakeArea = `, area1);
+/*
+ *
+ *
+ *
+ *
+ */
+// const num = 2;
+
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function containValue(value, arr) {
+  if (arr.indexOf(value)) {
+    console.log(`yes`);
+  } else {
+    console.log(`no`);
+  }
+  return num;
 }
-const area1 = calakeArea(5, 10);
-console.log(`calakeArea = `, area1);
+const num = containValue(2, 2);
+console.log(num);
+// arr.indexOf(num);
+// console.log(arr.indexOf(2));
